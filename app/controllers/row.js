304f9e7db2656d89ce32c2bc0b35e12a575b1e16
Matt Miquelon <miq@triometry.com>
2013-11-20 16:54:25 -0600

    Business view and merging with Adam's changes

--- app/controllers/row.js
+++ app/controllers/row.js
@@ -5,7 +5,7 @@ var day =  moment(args.eventData.nextOccurrence.start).utc().calendar().split(' 
 $.row.eventData = args.eventData;
 $.row.name = $.name.text = args.eventData.name;
 $.row.date = $.date.text = moment(args.eventData.nextOccurrence.start).utc().format('D');
-$.row.time = $.time.text = moment(args.eventData.nextOccurrence.start).utc().format('h a') +" - "+moment(args.eventData.nextOccurrence.end).utc().format('h a');
+$.row.time = $.time.text = moment(args.eventData.nextOccurrence.start).utc().format('h a') +"-"+moment(args.eventData.nextOccurrence.end).utc().format('h a');
 $.row.day = $.day.text =day;
 if(eventData.media != undefined && eventData.media.length >0){
 	$.detailImage.image = eventData.media[0].url;
