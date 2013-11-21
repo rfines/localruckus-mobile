d5070ce20530a4ff0ac1423c8e94174f520c66e2
Robert Fines <robert@localruckus.com>
2013-11-21 14:06:38 -0600

    added infinite scroll to table view

--- app/controllers/master.js
+++ app/controllers/master.js
@@ -23,39 +23,62 @@ function openSearchDrawer() {
 	drawerOpen = !drawerOpen;
 
 }
-
+var tag="ENTERTAINMENT";
+var reset = false;
+var page=0;
+var radius = 10000;
 function loadEntertainment(e) {
+	page = 0;
+	reset = true;
 	exports.loadInitialData({
 		tags : 'ENTERTAINMENT',
-		radius : 1000
+		radius : radius,
+		skip:0
 	});
 }
 
 function loadFood(e) {
+	page = 0;
+	reset = true;
+	tag = "FOOD-AND-DRINK";
 	exports.loadInitialData({
-		tags : 'FOOD-AND-DRINK',
-		radius : 1000
+		tags : tag,
+		radius : radius,
+		skip:0
 	});
+	
 }
 
 function loadMusic(e) {
+	page = 0;
+	reset = true;
+	tag = "MUSIC";
 	exports.loadInitialData({
-		tags : 'MUSIC',
-		radius : 1000
+		tags : tag,
+		radius : radius,
+		skip:0
 	});
 }
 
 function loadArts(e) {
+	page = 0;
+	reset = true;
+	tag = "ARTS";
 	exports.loadInitialData({
-		tags : 'ARTS',
-		radius : 1000
+		tags : tag,
+		radius : radius,
+		skip:0
 	});
 }
 
 function loadFamily(e) {
+	page = 0;
+	reset = true;
+	tag = "FAMILY-AND-CHILDREN";
 	exports.loadInitialData({
-		tags : "FAMILY-AND-CHILDREN",
-		radius : 1000
+		tags : tag,
+		radius : radius,
+		skip:0
 	});
 }
 
@@ -63,11 +86,15 @@ exports.loadInitialData = function(options) {
 	var data = [];
 	var moment = require('alloy/moment');
 	var options = options || {};
-	var radius = options.radius || 1000;
+	var radius = options.radius || radius;
 	var tags = options.tags || 'ENTERTAINMENT';
 	var ll = Alloy.Globals.location.coords.longitude + ',' + Alloy.Globals.location.coords.latitude;
 	var start = moment().toISOString();
-	var url = "http://api-stage.hoopla.io/event?ll=" + ll + "&radius=" + radius + "&tags=" + tags + "&height=150&imageType=circle&width=150&start=" + start;
+	var skip='';
+	if(options.skip){
+		skip = options.skip;
+	}
+	var url = "http://api-stage.hoopla.io/event?ll=" + ll + "&radius=" + radius + "&tags=" + tags + "&height=150&imageType=circle&width=150&start=" + start+"&limit=25&skip="+skip;
 	var xhr = Ti.Network.createHTTPClient({
 		onload : function(e) {
 			data = JSON.parse(this.responseText);
@@ -79,15 +106,20 @@ exports.loadInitialData = function(options) {
 					name : item.name
 				}).getView());
 			}
-			$.table.setData(tableData);
 			$.locationLabel.text = Alloy.Globals.reverseLocation.places[0].city + ', ' + Alloy.Globals.reverseLocation.places[0].zipcode;
 			$.addressTextField.value = Alloy.Globals.reverseLocation.places[0].address;
-			if(options.success){
-				options.success();
+			
+			if(!options.skip){
+				$.table.setData(tableData);
+			}else if(options.success){
+				options.success(tableData);
 			}
 		},
 		onerror : function(e) {
 			Ti.API.error(e);
+			if(options.error){
+				options.error();
+			}
 		}
 	});
 	Ti.API.error("Sending xhr request");
@@ -100,10 +132,35 @@ $.master.open();
 function myLoader(e) {
 	var el = e;
 	exports.loadInitialData({
-		tags : 'ENTERTAINMENT',
-		radius : 1000,
+		tags : tag,
+		radius : radius,
 		success : function() {
 			el.hide();
+		},
+		error : function(){
+			el.hide();
+		}
+	});
+}
+function loadMore(e){
+	var el = e;
+	page = page+1;
+	var skipNum=page*25;
+	exports.loadInitialData({
+		tags : tag,
+		radius : radius,
+		skip:skipNum,
+		success : function(data) {
+			$.table.appendRow(data);
+			if(data.length == 25){
+				el.success();
+			}else{
+				el.done();
+			}
+			
+		},
+		error : function(){
+			el.error();
 		}
 	});
 }
--- app/controllers/row.js
+++ app/controllers/row.js
@@ -1,12 +1,14 @@
 var args = arguments[0] || {};
 var moment = require('alloy/moment');
-var eventData = args.eventData;
-var day =  moment(args.eventData.nextOccurrence.start).utc().calendar().split(' ')[0];
-$.row.eventData = args.eventData;
-$.row.name = $.name.text = args.eventData.name;
-$.row.date = $.date.text = moment(args.eventData.nextOccurrence.start).utc().format('D');
-$.row.time = $.time.text = moment(args.eventData.nextOccurrence.start).utc().format('h a') +"-"+moment(args.eventData.nextOccurrence.end).utc().format('h a');
-$.row.day = $.day.text =day;
-if(eventData.media != undefined && eventData.media.length >0){
-	$.detailImage.image = eventData.media[0].url;
+if (args.eventData.nextOccurrence != undefined && args.eventData.nextOccurrence.start != undefined && args.eventData.nextOccurrence.end != undefined) {
+	var eventData = args.eventData;
+	var day = moment(args.eventData.nextOccurrence.start).utc().calendar().split(' ')[0];
+	$.row.eventData = args.eventData;
+	$.row.name = $.name.text = args.eventData.name;
+	$.row.date = $.date.text = moment(args.eventData.nextOccurrence.start).utc().format('D');
+	$.row.time = $.time.text = moment(args.eventData.nextOccurrence.start).utc().format('h a') + "-" + moment(args.eventData.nextOccurrence.end).utc().format('h a');
+	$.row.day = $.day.text = day;
+	if (eventData.media != undefined && eventData.media.length > 0) {
+		$.detailImage.image = eventData.media[0].url;
+	}
 }
