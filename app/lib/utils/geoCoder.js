31580db1b648c14c496afb00582cd1fecf132b1d
Adam England <adam.n.england@gmail.com>
2013-11-27 15:28:59 -0600

    Fixing geocoding issues

--- app/lib/utils/geoCoder.js
+++ app/lib/utils/geoCoder.js
@@ -1,5 +1,6 @@
 var flurry = require('sg.flurry');
 exports.myLocation = function(onError, onSuccess) {
+	Ti.Geolocation.accuracy = Ti.Geolocation.ACCURACY_HIGH;
 	if (Ti.Geolocation.locationServicesEnabled) {
 		Titanium.Geolocation.purpose = 'Get Current Location';
 		Titanium.Geolocation.getCurrentPosition(function(e) {
@@ -8,16 +9,7 @@ exports.myLocation = function(onError, onSuccess) {
 			} else {
 				Alloy.Globals.location = e;
 				flurry.setLatitude(e.coords.latitude, e.coords.longitude);
-				Titanium.Geolocation.reverseGeocoder(e.coords.latitude, e.coords.longitude, function(reverseGeocoderResonse) {
-					if (reverseGeocoderResonse.code == 0 && reverseGeocoderResonse.places[0] != undefined) {
-						Alloy.Globals.displayAddress = reverseGeocoderResonse.places[0].address;
-						Alloy.Globals.cityState = reverseGeocoderResonse.places[0].city + ' - ' + reverseGeocoderResonse.places[0].zipcode;
-					} else {
-						Alloy.Globals.displayAddress = '';
-						Alloy.Globals.cityState = "Location Unknown";
-					}
-					onSuccess();
-				});
+				reverse(e.coords.latitude, e.coords.longitude, onSuccess);
 			}
 		});
 	} else {
@@ -26,23 +18,78 @@ exports.myLocation = function(onError, onSuccess) {
 };
 
 exports.customLocation = function(address, onError, onSuccess) {
-	Titanium.Geolocation.forwardGeocoder(address, function(forwardGeocoderResponse) {
-		if (forwardGeocoderResponse.error !=undefined && forwardGeocoderResponse.error.length >0) {
-			Ti.API.error(forwardGeocoderResponse.error);
-			onError(forwardGeocoderResponse);
-		} else {
-			Alloy.Globals.location.coords.longitude = forwardGeocoderResponse.longitude;
-			Alloy.Globals.location.coords.latitude = forwardGeocoderResponse.latitude;
-			Titanium.Geolocation.reverseGeocoder(forwardGeocoderResponse.latitude, forwardGeocoderResponse.longitude, function(reverseGeocoderResonse) {
-				if (reverseGeocoderResonse.code == 0 && reverseGeocoderResonse.places[0] != undefined) {
-					Alloy.Globals.displayAddress = address;
-					Alloy.Globals.cityState = reverseGeocoderResonse.places[0].city + ' - ' + reverseGeocoderResonse.places[0].zipcode;
-				} else {
-					Alloy.Globals.displayAddress = address;
-					Alloy.Globals.cityState = "Location Unknown";
-				}
-				onSuccess();
-			});
+	var addrUrl = "http://maps.googleapis.com/maps/api/geocode/json?sensor=true&address="+encodeURIComponent(address);
+	var addrReq = Titanium.Network.createHTTPClient({
+		onload: function() {
+			var response = JSON.parse(this.responseText);
+			if (response.status == 'OK' && response.results && response.results.length > 0) {
+				Alloy.Globals.location.coords.longitude = response.results[0].geometry.location.lng;
+				Alloy.Globals.location.coords.latitude = response.results[0].geometry.location.lat;
+				Alloy.Globals.displayAddress = address;
+				reverse(Alloy.Globals.location.coords.latitude, Alloy.Globals.location.coords.longitude, onSuccess);
+			} else {
+				onError(forwardGeocoderResponse);
+			}
+			onSuccess();
 		}
-	});
+	});		
+	addrReq.open("GET",addrUrl);	
+	addrReq.send();
+};
+
+
+function reverse(lat, lng, onSuccess) {
+	var addrUrl = "http://maps.googleapis.com/maps/api/geocode/json?sensor=true&latlng="+lat+","+lng;
+	var addrReq = Titanium.Network.createHTTPClient({
+		onload: function() {
+			var response = JSON.parse(this.responseText);
+			if (response.status == 'OK' && response.results && response.results.length > 0) {
+				Alloy.Globals.displayAddress = response.results[0].formatted_address;
+				Alloy.Globals.cityState = getCityState(response);
+			} else {
+				Alloy.Globals.cityState = "Location Unknown";
+			}
+			onSuccess();
+		}
+	});		
+	addrReq.open("GET",addrUrl);	
+	addrReq.send();
+};
+
+function getCityState(response) {
+	var address = {
+	   streetName      :'Not Found',
+	   streetNumber    :'',
+	   city            :'',
+	   country         :'',
+	   stateName       :'',
+	   postalCode      :'',
+	   latitude      :'',
+	   longitude     :''
+	};
+   var resLen = response.results[0].address_components.length;
+   for(var i=0; i < resLen; i++) {
+       switch (response.results[0].address_components[i].types[0])
+       {
+           case "street_number":
+               address.streetNumber  = response.results[0].address_components[i].long_name;
+               break;
+           case "route":
+               address.streetName    = response.results[0].address_components[i].long_name;
+               break;
+           case "locality":
+               address.city          = response.results[0].address_components[i].long_name;
+               break;
+           case "administrative_area_level_1":
+               address.stateName     = response.results[0].address_components[i].long_name;
+               break;
+           case "postal_code":
+               address.postalCode    = response.results[0].address_components[i].long_name;
+               break;
+           case "country":
+               address.country       = response.results[0].address_components[i].long_name;
+               break;
+           }
+   }
+   return address.city + ', ' + address.stateName + ' ' + address.postalCode;
 };
